1c3b48e1274a2fab028bca525af2ac52a16dd4e6
mlknz <rkdshe@gmail.com>
2018-06-12 03:17:02 +0300

    Add multiple rays and debug raycast visual

--- src/game/player/player.gd
+++ src/game/player/player.gd
@@ -1,16 +1,38 @@
 extends Spatial
 
-var playerMesh
+var playerBody
+var isDebug = OS.is_debug_build()
+
+# todo: move to separate module
+var debugRaycastSphereSize = 0.2
+var debugSpheres = []
+var debugSticks = []
 
 func _ready():
-	# Called every time the node is added to the scene.
-	#playerMesh = get_node("PlayerMesh")
-	pass
+	playerBody = get_node("RigidBody")
+	
+	if (isDebug && playerBody):
+		for i in range(playerBody.thrustSpots.size()):
+			debugSpheres.append(MeshInstance.new())
+			debugSpheres[i].mesh = SphereMesh.new()
+			debugSpheres[i].mesh.radius = debugRaycastSphereSize
+			debugSpheres[i].mesh.height = debugRaycastSphereSize * 2.0
+			playerBody.add_child(debugSpheres[i])
+		
+			debugSticks.append(MeshInstance.new())
+			debugSticks[i].mesh = CylinderMesh.new()
+			debugSticks[i].mesh.bottom_radius = debugRaycastSphereSize * 0.1
+			debugSticks[i].mesh.top_radius = debugRaycastSphereSize * 0.1
+			playerBody.add_child(debugSticks[i])
+	
 
 func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
+	if (isDebug && playerBody):
+		for i in range(debugSpheres.size()):
+			debugSpheres[i].translation = playerBody.thrustSpots[i] + Vector3(0, -playerBody.thrustHitDistances[i], 0)
+			debugSticks[i].mesh.height = playerBody.thrustHitDistances[i]
+			debugSticks[i].translation = playerBody.thrustSpots[i] + Vector3(0, -playerBody.thrustHitDistances[i] * 0.5, 0)
 	pass
-	
+
 func _physics_process(delta):
 	
--- src/game/player/playerRigidBody.gd
+++ src/game/player/playerRigidBody.gd
@@ -3,6 +3,10 @@ extends RigidBody
 var velocity = Vector3(0, 0, 0)
 var g = 9.8
 
+var thrustSpots = [Vector3(-0.5, 0, 0.5), Vector3(0.5, 0, 0.5), Vector3(-0.5, 0, -0.5), Vector3(0.5, 0, -0.5)]
+var thrustOrientations = [Vector3(0, 0, 0), Vector3(0, 0, 0), Vector3(0, 0, 0), Vector3(0, 0, 0)] # unused
+var thrustHitDistances = [0, 0, 0, 0]
+
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
@@ -14,18 +18,17 @@ func _ready():
 #	pass
 
 func _physics_process(delta):
-	
 	var spaceState = get_world().direct_space_state
-	var objectPosition = translation
-	var result = spaceState.intersect_ray(objectPosition, objectPosition + Vector3(0, -8, 0))
-	var hitDist = -1;
-	if result:
-		hitDist = objectPosition.distance_to(result.position)
-	
-	# [0.0, 2.0]
-	var acc = max((2.0 - hitDist), 0.0)
-	acc = acc * delta * g
-	apply_impulse(Vector3(0, 0, 0), Vector3(0, acc, 0))
+
+	for i in range(thrustSpots.size()):	
+		var result = spaceState.intersect_ray(translation + thrustSpots[i]
+				, translation + thrustSpots[i] - 8 * global_transform.basis.y)
+		thrustHitDistances[i] = translation.distance_to(result.position) if result else 0
+		
+		# draft broken logic
+		var acc = max((1.0 - thrustHitDistances[i]), 0.0)
+		acc = acc * delta * g
+		apply_impulse(thrustSpots[i], Vector3(0, acc, 0))
 
 	var controls = Vector3(0, 0, 0)
 	if Input.is_action_pressed("ui_right"):
@@ -41,7 +44,7 @@ func _physics_process(delta):
 	apply_impulse(Vector3(0, 0, 0), controls)
 
 func _integrate_forces(state):
-	print(state.angular_velocity.x, " ", state.angular_velocity.y,
-	" ", state.angular_velocity.z)
+	#print(state.angular_velocity.x, " ", state.angular_velocity.y,
+	#" ", state.angular_velocity.z)
 	#print(state.total_angular_damp)
 	pass
