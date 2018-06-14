b1c2820a216a0667a1c5004ffbfd151416e9ee18
mlknz <rkdshe@gmail.com>
2018-06-15 01:47:00 +0300

    Unstable hovering logic
    With 4 raycasts down

--- src/game/player/Camera.gd
+++ src/game/player/Camera.gd
@@ -1,19 +1,25 @@
 extends Camera
 
 var parent
+var player
 var prevMousePos = Vector2(-1, -1)
 var phi
 var theta
 var r
 
+var originPos
+
 func _ready():
 	parent = get_parent_spatial()
-	r = translation.length()
-	phi = atan2(translation.x, translation.y)
-	theta = acos(translation.z/r)
+	player = parent.get_node("RigidBody")
+	originPos = player.translation
+	var tr = translation - originPos
+	r = tr.length()
+	phi = atan2(tr.x, tr.y)
+	theta = acos(tr.z/r)
 	pass
 
-func _process(delta):
+func _process(delta):	
 	if Input.is_action_pressed("ui_mouse_left"):
 		var v = get_viewport()
 		var mousePos = v.get_mouse_position() / v.size
@@ -21,9 +27,13 @@ func _process(delta):
 			var diff = mousePos - prevMousePos
 			theta -= diff.y * 2
 			phi -= diff.x * 2
-			translation = r * Vector3(cos(theta) * sin(phi), sin(theta) * sin(phi), cos(phi))
-			look_at(parent.global_transform.origin, Vector3(0, 1, 0))
 		prevMousePos = mousePos
 	else:
 		prevMousePos.x = -1
+	
+	var targetPos = Vector3(player.translation.x, originPos.y, player.translation.z)
+	translation = r * Vector3(cos(theta) * sin(phi), sin(theta) * sin(phi), cos(phi)) + targetPos
+	var originWorld = player.global_transform.origin
+	var lookAtPos = Vector3(originWorld.x, 1, originWorld.z)
+	look_at(lookAtPos, Vector3(0, 1, 0))
 
--- src/game/player/playerRigidBody.gd
+++ src/game/player/playerRigidBody.gd
@@ -6,44 +6,47 @@ var g = 9.8
 var thrustSpots = []
 var thrustOrientations = [] # unused
 var thrustHitDistances = [0, 0, 0, 0]
+var thrusterStrength = 4
+var thrusterDist = 6
 
 func _ready():
 	var s = get_node("CollisionShape").shape.extents
-	thrustSpots = [Vector3(-s.x, 0, s.z), Vector3(s.x, 0, s.z), Vector3(-s.x, 0, -s.z), Vector3(s.x, 0, -s.z)]
-	# Called every time the node is added to the scene.
-	# Initialization here
+	var y = -s.y
+	thrustSpots = [Vector3(-s.x, y, s.z), Vector3(s.x, y, s.z), Vector3(-s.x, y, -s.z), Vector3(s.x, y, -s.z)]
+	
+	angular_damp = 0.7 # seems we need custom damp
 	pass
 
 #func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
 #	pass
 
 func _physics_process(delta):
 	var spaceState = get_world().direct_space_state
-
+	var up = global_transform.basis.y.normalized()
+	#print(up.x, " ", up.y, " ", up.z)
 	for i in range(thrustSpots.size()):
 		var tr = translation + thrustSpots[i]
-		var result = spaceState.intersect_ray(tr, tr - 8 * global_transform.basis.y, [self], collision_mask)
+		var result = spaceState.intersect_ray(tr, tr - thrusterDist * up)
 		thrustHitDistances[i] = translation.distance_to(result.position) if result else 0
 		
-		# draft broken logic
-		var acc = max((0.5 - thrustHitDistances[i]), 0.0)
-		acc = acc * delta * g
-		apply_impulse(thrustSpots[i], acc * global_transform.basis.y)
+		var acc = 1.0 - min(thrustHitDistances[i] / thrusterDist, 1.0)
+		#print(acc)
+		acc = pow(acc, 2) * delta * thrusterStrength * mass
+		apply_impulse(thrustSpots[i], acc * up)
 
 	var controls = Vector3(0, 0, 0)
 	if Input.is_action_pressed("ui_right"):
-		controls.x = 10 * delta
+		controls.x = 5 * delta * mass
 	if Input.is_action_pressed("ui_left"):
-		controls.x = -10 * delta
+		controls.x = -5 * delta * mass
 	if Input.is_action_pressed("ui_up"):
-		controls.z = -10 * delta
+		controls.z = -5 * delta * mass
 	if Input.is_action_pressed("ui_down"):
-		controls.z = 10 * delta
+		controls.z = 5 * delta * mass
 	#if Input.is_action_pressed("ui_accept"):
 	
-	apply_impulse(Vector3(0, 0, 0), controls)
+	apply_impulse(Vector3(0, 0, 0), global_transform.basis.z * controls.z)
+	apply_impulse(Vector3(0, 0, 0), global_transform.basis.x * controls.x)
 
 func _integrate_forces(state):
 	#print(state.angular_velocity.x, " ", state.angular_velocity.y,
